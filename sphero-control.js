a7ceb88cc855100e5b15fa39d2694ba2b57adbe7
Devin Mui <devinmui@yahoo.com>
2016-08-30 21:28:12 -0700

    lol forgot to remote comments

--- sphero-control.js
+++ sphero-control.js
@@ -1,5 +1,5 @@
-// var sphero = require("sphero"),
-//     bb8 = sphero("/dev/tty.Sphero-PWR-AMP-SPP"); // change BLE address accordingly
+var sphero = require("sphero"),
+    bb8 = sphero("/dev/tty.Sphero-PWR-AMP-SPP"); // change BLE address accordingly
 
 var express = require('express')
 
@@ -43,29 +43,29 @@ app.listen(3000, function () {
 
 // sphero
 
-// bb8.connect(function() {
-//   setInterval(function() {
-//   	if(direction === 'stop'){
-//   		console.log('stop')
-//   	} else if (direction == 'forward'){
-//   		console.log('forward')
-//   		var angle = 180
-//     	bb8.roll(150, angle);
-//   	} else if (direction == 'backward'){
-//   		console.log('backward')
-//   		var angle = 0
-//     	bb8.roll(150, angle);
-//   	} else if (direction == 'left') {
-//   		console.log('left')
-// 			var angle = 90
-//     	bb8.roll(150, angle);
-//   	} else if (direction == 'right') {
-//   		console.log('right')
-//   		var angle = 270
-//     	bb8.roll(150, angle);
-//   	} else if (direction == 'kill'){
-//   		console.log('kill')
-//   		clearInterval()
-//   	}
-//   }, 1000);
-// });
+bb8.connect(function() {
+  setInterval(function() {
+  	if(direction === 'stop'){
+  		console.log('stop')
+  	} else if (direction == 'forward'){
+  		console.log('forward')
+  		var angle = 180
+    	bb8.roll(150, angle);
+  	} else if (direction == 'backward'){
+  		console.log('backward')
+  		var angle = 0
+    	bb8.roll(150, angle);
+  	} else if (direction == 'left') {
+  		console.log('left')
+			var angle = 90
+    	bb8.roll(150, angle);
+  	} else if (direction == 'right') {
+  		console.log('right')
+  		var angle = 270
+    	bb8.roll(150, angle);
+  	} else if (direction == 'kill'){
+  		console.log('kill')
+  		clearInterval()
+  	}
+  }, 1000);
+});
